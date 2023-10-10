646d2915006afa7b592b526aca36bd3a614e786c
MARIA APARECIDA GOMES <20221174010022@ifrn.local>
2023-10-10 17:53:51 -0300

    first commit

---
+++ diamante.py
@@ -0,0 +1,11 @@
+import turtle
+
+ste = turtle.Turtle()
+
+ste.forward(150)
+ste.left(120)
+ste.forward(150)
+ste.left(60)
+ste.forward(150)
+ste.left(120)
+ste.forward(150)
---
+++ quadradro.py
@@ -0,0 +1,11 @@
+import turtle
+
+ste = turtle .Turtle()
+
+ste.forward(100)
+ste.right(90)
+ste.forward(100)
+ste.right(90)
+ste.forward(100)
+ste.right(90)
+ste.forward(100)
